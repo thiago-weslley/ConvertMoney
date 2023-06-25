9690362fbe8ae314b7ef7ec2c4cfb528b7abb2d9
Thiago Weslley <thiagoweslley@yahoo.com>
2023-06-24 21:54:55 -0300

    feat: adiciona API de conversão monetária

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,37 +2,40 @@ const convertButton = document.querySelector('#convert-button');
 const selectConvertFor = document.querySelector('#convert-for');
 const clearButton = document.querySelector('#clear-button');
 
-const convertValues = () => {
+const convertValues = async () => {
     const inputConversionValue = document.querySelector('#conversion-value').value;
     const inputConversionResult = document.querySelector('#conversion-result');
 
+    const data = await fetch('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,GBP-BRL,BTC-BRL').then(response => response.json());
+
+    const dolarToday = data.USDBRL;
+    const euroToday = data.EURBRL;
+    const libraToday = data.GBPBRL;
+    const bitcoinToday = data.BTCBRL;
+
     if (selectConvertFor.value === 'dolar') {
-        const dolarToday = 4.92;
         inputConversionResult.value = new Intl.NumberFormat('en-US', {
             style: 'currency',
             currency: 'USD'
-        }).format(inputConversionValue / dolarToday);
+        }).format(inputConversionValue / dolarToday.high);
     }
 
     if (selectConvertFor.value === 'euro') {
-        const euroToday = 5.30;
         inputConversionResult.value = new Intl.NumberFormat('en-GB', {
             style: 'currency',
             currency: 'EUR'
-        }).format(inputConversionValue / euroToday);
+        }).format(inputConversionValue / euroToday.high);
     }
 
     if (selectConvertFor.value === 'libra') {
-        const libraToday = 6.18;
         inputConversionResult.value = new Intl.NumberFormat('en-GB', {
             style: 'currency',
             currency: 'GBP'
-        }).format(inputConversionValue / libraToday);
+        }).format(inputConversionValue / libraToday.high);
     }
 
     if (selectConvertFor.value === 'bitcoin') {
-        const bitcoinToday = 130028.61;
-        inputConversionResult.value = (inputConversionValue / bitcoinToday).toFixed(8) + " BTC";
+        inputConversionResult.value = (inputConversionValue / bitcoinToday.high).toFixed(8) + " BTC";
     }
 }
 
@@ -56,7 +59,7 @@ const clearInput = () => {
         inputConversionResult.value = '£0.00';
     }
 
-    if (selectConvertFor.value === 'bitcoin'){
+    if (selectConvertFor.value === 'bitcoin') {
         inputConversionResult.value = '0.00000000 BTC';
     }
 
